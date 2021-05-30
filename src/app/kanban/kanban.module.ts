e50fcf14ef9dc1554cd38ab812a41dd669d56c61
Marcio Barboza <marciocleberbrazil@gmail.com>
2021-05-30 18:37:14 +1000

    Kanban Module

---
+++ src/app/kanban/kanban.module.ts
@@ -0,0 +1,27 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SharedModule } from '../shared/shared.module';
+import { BoardListComponent } from './board-list/board-list.component';
+import { KanbanRoutingModule } from './kanban-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    BoardListComponent
+  ],
+  imports: [
+    CommonModule,
+    KanbanRoutingModule,
+    SharedModule,
+    FormsModule,
+    DragDropModule,
+    MatDialogModule,
+    MatButtonToggleModule
+  ]
+})
+export class KanbanModule { }
