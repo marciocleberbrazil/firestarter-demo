38e3a84d509037dc68d883353f3e22e5ede92ca9
Marcio Barboza <marciocleberbrazil@gmail.com>
2021-05-30 17:54:57 +1000

    Email Password Auth

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { UserRoutingModule } from './user-routing.module';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { EmailLoginComponent } from './email-login/email-login.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserRoutingModule } from './user-routing.module';
+
 
 
 @NgModule({
   declarations: [
     LoginPageComponent,
-    GoogleSigninDirective
+    GoogleSigninDirective,
+    EmailLoginComponent
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
